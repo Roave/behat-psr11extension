ee88d4a3299a1b78c82d724b49f93164cda28efd
James Titcumb <james@asgrim.com>
2017-08-16 11:22:09 +0100

    Added return type declarations

--- src/PsrContainerExtension.php
+++ src/PsrContainerExtension.php
@@ -7,28 +7,27 @@ use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class PsrContainerExtension implements Extension
 {
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container) : void
     {
     }
 
-    public function getConfigKey()
+    public function getConfigKey() : string
     {
         return __NAMESPACE__;
     }
 
-    public function initialize(ExtensionManager $extensionManager)
+    public function initialize(ExtensionManager $extensionManager) : void
     {
     }
 
-    public function configure(ArrayNodeDefinition $builder)
+    public function configure(ArrayNodeDefinition $builder) : void
     {
+        /** @noinspection NullPointerExceptionInspection */
         $builder
             ->children()
                 ->scalarNode('container')->defaultValue('config/container.php')->end()
@@ -37,7 +36,7 @@ final class PsrContainerExtension implements Extension
         ->end();
     }
 
-    public function load(ContainerBuilder $container, array $config)
+    public function load(ContainerBuilder $container, array $config) : void
     {
         $container->setParameter('roave.behat.psr.container.included.file', $config['container']);
         $container->setDefinition($config['name'], $this->createContainerDefinition());
@@ -45,7 +44,7 @@ final class PsrContainerExtension implements Extension
 
     private function createContainerDefinition() : Definition
     {
-        $definition = new Definition(ContainerInterface::class, ["%roave.behat.psr.container.included.file%"]);
+        $definition = new Definition(ContainerInterface::class, ['%roave.behat.psr.container.included.file%']);
         $definition->setFactory([ContainerFactory::class, 'createContainerFromIncludedFile']);
         $definition->addTag('helper_container.container');
 
@@ -58,8 +57,10 @@ final class PsrContainerExtension implements Extension
      * The way to set service scope was improved across Symfony versions:
      *  - setShared was introduced in 2.8
      *  - setScope (and the constant) were removed in 3.0
+     *
+     * @param Definition $definition
      */
-    private function setContainerScope($definition) : void
+    private function setContainerScope(Definition $definition) : void
     {
         if (method_exists($definition, 'setShared')) {
             $definition->setShared(false);
