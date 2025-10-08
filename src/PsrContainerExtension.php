8d795cb484e7696acfe1c8a5bde3479021c2b393
Marco Pivetta <ocramius@gmail.com>
2025-10-08 16:46:52 +0200

    Simplified `PsrContainerExtension` to comply with `symfony/dependency-injection:^7`
    Solves:
    
    * one excessive `->end()` call
    * unused duck-typing around `setShared()` API (introduced in `symfony/*:^2.8`)

--- src/PsrContainerExtension.php
+++ src/PsrContainerExtension.php
@@ -11,8 +11,6 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 
-use function method_exists;
-
 final class PsrContainerExtension implements Extension
 {
     public function process(ContainerBuilder $container): void
@@ -35,8 +33,7 @@ final class PsrContainerExtension implements Extension
             ->children()
                 ->scalarNode('container')->defaultValue('config/container.php')->end()
                 ->scalarNode('name')->defaultValue('psr_container')->end()
-            ->end()
-        ->end();
+            ->end();
     }
 
     /** @param string[] $config */
@@ -52,23 +49,8 @@ final class PsrContainerExtension implements Extension
         $definition->setFactory([ContainerFactory::class, 'createContainerFromIncludedFile']);
         $definition->addTag('helper_container.container');
         $definition->setPublic(true);
-
-        $this->setContainerScope($definition);
+        $definition->setShared(false);
 
         return $definition;
-    }
-
-    /**
-     * The way to set service scope was improved across Symfony versions:
-     *  - setShared was introduced in 2.8
-     *  - setScope (and the constant) were removed in 3.0
-     */
-    private function setContainerScope(Definition $definition): void
-    {
-        if (method_exists($definition, 'setShared')) {
-            $definition->setShared(false);
-        } else {
-            $definition->setScope(ContainerBuilder::SCOPE_PROTOTYPE);
-        }
     }
 }
